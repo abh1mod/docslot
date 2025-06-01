8b5418a33faa3e0563167658bd19351e4bdd955a
abh1mod <absid1120@gmail.com>
2025-06-01 12:45:11 +0530

    Routes between login page created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css'
-import Registration from './Components/Registration'
+import RegistrationDoc from './Pages/LoginPages/RegistrationDoc';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginDoc from './Pages/LoginPages/LoginDoc';
+import DoctorProfile from './Pages/DoctorProfile';
+import HomePage from './Pages/HomePage';
+import RegistrationPt from './Pages/LoginPages/RegistrationPt'
+import LoginPt from './Pages/LoginPages/LoginPt';
 function App() {
   return (
-    <>
-     <Registration/>
-    </>
+    <div>
+  <Router>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/registration_doc" element={<RegistrationDoc/>}/>   
+            <Route path="/login_doc" element={<LoginDoc/>} />
+            <Route path="/doctor/:doc_id" element={<DoctorProfile />} />
+            <Route path="/registration_pt" element={<RegistrationPt/>}/>
+            <Route path="/login_pt" element={<LoginPt/>}/>
+        </Routes>
+    </Router>
+    </div>
+    
+    
   )
 }
 
